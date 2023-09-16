12c12e46f8445d7bf00050dbac6afcad37367529
Amit Yadav <yamit5869@protonmail.com>
2023-09-17 00:05:44 +0530

    minor change

--- project/settings.py
+++ project/settings.py
@@ -130,7 +130,7 @@ if not DEBUG:
     STATICFILES_DIRS = [BASE_DIR / "static"]  
     STATIC_ROOT = BASE_DIR / "staticfiles"  # new
     # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
