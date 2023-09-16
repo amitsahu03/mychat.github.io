2373f5572ba089fdb7634123a8cc7ca5ed691080
Amit Yadav <yamit5869@protonmail.com>
2023-09-16 23:33:19 +0530

    minor change

--- project/settings.py
+++ project/settings.py
@@ -129,6 +129,7 @@ STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 # Default primary key field type
